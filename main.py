b2fcbdedb3bf96713fa3256132bcd53cd9a961ac
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-15 11:10:12 -0500

    Start Window class

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("aMAZEment")
+        self.canvas = Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.window_running = False
