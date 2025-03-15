e4a19669719c4585b7e1c5eaff62c9015b3eb079
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-15 11:20:26 -0500

    Add wait and close methods

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title("aMAZEment")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
         self.canvas.pack()
         self.window_running = False
@@ -15,4 +16,10 @@ class Window:
         self.root.update_idletasks()
         self.root.update()
 
-    
+    def wait_for_close(self):
+        self.window_running = True
+        while self.window_running:
+            self.redraw()
+
+    def close(self):
+        self.window_running = False
