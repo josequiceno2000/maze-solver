45e4a1b439469c708d9ea6387fa6f62532185702
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-15 12:49:52 -0500

    Add draw_move method to cell

--- cell.py
+++ cell.py
@@ -21,3 +21,24 @@ class Cell:
             self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
+    
+    def draw_move(self, to_cell, undo=False):
+        # Set color
+        if undo:
+            fill_color = "gray"
+        elif not undo:
+            fill_color = "red"
+
+        # Get center of this cell
+        this_center_x = (self._x1 + self._x2) / 2
+        this_center_y = (self._y1 + self._y2) / 2
+
+        # Get center for other cell
+        to_center_x = (to_cell._x1 + to_cell._x2) / 2
+        to_center_y = (to_cell._y1 + to_cell._y2) / 2
+
+        # Draw line
+        center_line = Line(Point(this_center_x, this_center_y), Point(to_center_x, to_center_y))
+        self._win.draw_line(center_line, fill_color)
+        
+
